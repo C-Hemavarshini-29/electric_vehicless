32a33744d93d858926c5c51ad58484fa303a4917
C-Hemavarshini-29 <128209069+C-Hemavarshini-29@users.noreply.github.com>
2023-04-18 11:27:07 +0530

    Add files via upload

---
+++ electric india.sql
@@ -0,0 +1,4 @@
+SELECT * FROM electric_car.`cheapestelectriccars-evdatabase`;
+SELECT * FROM electric_car.`electric_vehicle_charging_station_list`;
+SELECT * FROM electric_car.`electriccardata_clean`;
+SELECT * FROM electric_car.`evindia`;
